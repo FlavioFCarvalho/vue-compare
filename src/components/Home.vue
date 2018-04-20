d4c87db1a847528782cb84ad41b739fa5ef317cc
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-04-20 14:31:42 -0300

    Supporting Content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,6 +11,48 @@
         </div>
       </div>
     </section>
+     <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+          <div class="column is-1 has-text-centered">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+          <div class="column is-one-third-desktop">
+            <p class="title"><strong>We provide superior logistics so that your business can succeed in a crazy world.</strong></p>
+          </div>
+          <div class="column">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</strong></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+              <p class="subtitle">- Gary Simon</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
